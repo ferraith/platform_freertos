6a98d071b06bb21fd7b10f3ecddccffedec41f98
ferraith <ferraith@gmail.com>
2012-12-25 15:53:16 +0100

    Added own implementation of new and delete operators

---
+++ src/rtos_del.cpp
@@ -0,0 +1,28 @@
+// FreeRTOS Library
+// Copyright (C) ferraith. All rights reserved.
+
+#include <cstddef>
+
+#include "FreeRTOS.h"
+
+
+///
+/// @brief         Deallocates storage. This allocation function is called by delete-expressions to deallocate memory
+///                after destructing a scalar object.
+/// @param[in]     ptr  Pointer to a memory area to deallocate or a null pointer
+/// @return        None
+///
+void operator delete(void *ptr) throw() {
+  vPortFree(ptr);
+}
+
+
+///
+/// @brief         Deallocates storage. This allocation function are called by delete-expressions to deallocate memory
+///                after destructing an array of dynamic objects.
+/// @param[in]     ptr  Pointer to a memory area to deallocate or a null pointer
+/// @return        None
+///
+void operator delete[](void *ptr) throw() {
+  vPortFree(ptr);
+}
