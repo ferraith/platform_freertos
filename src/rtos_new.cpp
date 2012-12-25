3a1babd0241e49b87b65318f7a92265f6edc1f7a
ferraith <ferraith@gmail.com>
2012-12-25 15:53:16 +0100

    Added own implementation of new and delete operators

---
+++ src/rtos_new.cpp
@@ -0,0 +1,28 @@
+// FreeRTOS Library
+// Copyright (C) ferraith. All rights reserved.
+
+#include <cstddef>
+
+#include "FreeRTOS.h"
+
+
+///
+/// @brief         Allocates requested number of bytes. This allocation function is called by new-expressions to
+///                allocate memory in which a new object would then be initialized.
+/// @param[in]     Size  Number of bytes to allocate
+/// @return        Pointer to allocated memory area
+///
+void *operator new(std::size_t size) throw () {
+  return pvPortMalloc(size);
+}
+
+
+///
+/// @brief         Allocates requested number of bytes. This allocation function is called by new-expressions to
+///                allocate memory in which an array of new objects would then be initialized.
+/// @param[in]     size  Number of bytes to allocate
+/// @return        Pointer to allocated memory area
+///
+void *operator new[](std::size_t size) throw () {
+  return pvPortMalloc(size);
+}
